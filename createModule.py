cd146ee613318647645dd17ffafc74b68a844e82
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-05-13 00:27:13 +0300

    Create createModule.py

---
+++ createModule.py
@@ -0,0 +1,23 @@
+''' modul hakkında bilgilendirme
+'''
+
+print('modul başarıyla eklendi.')
+number=10
+
+numbers=[1,2,3]
+
+person = {
+
+    "name": "Sercan",
+    "age":"24",
+    "city":"istanbul"
+}
+
+def func(x):
+    '''
+        fonksiyon hakkında bilgilendirme
+    '''
+
+class Person:
+    def speak(self):
+        print('I speak English.')
