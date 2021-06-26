ec6c3bb034f7451068c5ee242014f1e0d9e272d0
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-06-27 01:36:05 +0300

    Add files via upload

---
+++ generators.py
@@ -0,0 +1,13 @@
+def cube():
+    for i in range(5):
+        yield i**3
+
+
+generator=cube()
+iterator=iter(generator)
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
