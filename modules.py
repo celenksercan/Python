c0008852d7210f495faddc86d119881b55cbcda8
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-07-01 00:59:47 +0300

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,19 @@
+#yöntem 1
+
+""" import math as islem
+
+
+value=islem.sqrt(121)
+
+value2=islem.ceil(6.6)
+print(value)
+print(value2)
+ """
+
+ #yöntem 2 tüm özellikleri import eder,
+ #artık math veya islem yazmaya gerek yoktur.
+from math import *
+
+
+sayi=sqrt(25)
+print(sayi)
