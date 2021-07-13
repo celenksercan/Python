a0b970f43fc8fdd9c0ee89c6cf4b89eea751908a
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-07-13 23:54:27 +0300

    Add files via upload

---
+++ numpyModules2.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+result1=np.arange(1,10)
+result2=np.arange(10,40,3)
+result3=np.zeros(5)
+result4=np.linspace(0,100,5)
+result5=np.random.randint(0,50)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
